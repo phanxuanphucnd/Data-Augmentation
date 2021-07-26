7952cd2cfa97ede69531d558c629b0c1b54bf7af
phanxuanphucnd <phanxuanphucnd@gmail.com>
2021-07-27 01:23:45 +0700

    update

--- arizona/textmentations/augmentation.py
+++ arizona/textmentations/augmentation.py
@@ -2,17 +2,24 @@
 
 import os
 
+from pandas import DataFrame
 from typing import List, Text, Union
 
+from arizona.textmentations.functions import *
+
 class TextAugmentation:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, data: DataFrame=None, text_col) -> None:
+        super(TextAugmentation, self).__init__()
+        
+        self.data = data
     
     def augment(
         self, 
         methods: Union[List[dict], dict], 
-        export_folder: Text='./data/',
+        export_dir: Text='./output/',
         export_file: Text='aug_data.csv'
     ):
+        if not os.path.exists(export_dir):
+            os.makedirs(export_dir)
 
         raise NotImplementedError
