b161158d9b5866be305263c205d264456eae17df
phanxuanphucnd <phanxuanphucnd@gmail.com>
2021-07-26 17:31:27 +0700

    update func remove accent

--- arizona/textmentations/functions.py
+++ arizona/textmentations/functions.py
@@ -1,11 +1,27 @@
 # -*- coding: utf-8 -*-
 # Provide function to augmentation data
 
+import random
+
 from typing import Text
 
 def abbreviates_func(text: Text, replace_prob: float=0.5):
+    s1 = u'ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚÝàáâãèéêìíòóôõùúýĂăĐđĨĩŨũƠơƯưẠạẢảẤấẦầẨẩẪẫẬậẮắẰằẲẳẴẵẶặẸẹẺẻẼẽẾếỀềỂểỄễỆệỈỉỊịỌọỎỏỐốỒồỔổỖỗỘộỚớỜờỞởỠỡỢợỤụỦủỨứỪừỬửỮữỰựỲỳỴỵỶỷỸỹ'
+    s0 = u'AAAAEEEIIOOOOUUYaaaaeeeiioooouuyAaDdIiUuOoUuAaAaAaAaAaAaAaAaAaAaAaAaEeEeEeEeEeEeEeEeIiIiOoOoOoOoOoOoOoOoOoOoOoOoUuUuUuUuUuUuUuYyYyYyYy'
+    
+    output = ''
+    for char in text:
+        if char in s1:
+            rd = random.random()
+            print(rd)
+            if rd >= replace_prob:
+                output += s0[s1.index(char)]
+            else:
+                output += char
+        else:
+            output += char
 
-    return
+    return output
 
 def remove_accent_func(text: Text, replace_prob: float=0.5):
 
