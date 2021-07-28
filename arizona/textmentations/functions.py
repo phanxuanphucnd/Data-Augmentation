80cd1c53ecae9d4c95abf9c56164a12a370b4e26
phanxuanphucnd <phanxuanphucnd@gmail.com>
2021-07-28 15:13:16 +0700

    update

--- arizona/textmentations/functions.py
+++ arizona/textmentations/functions.py
@@ -4,6 +4,7 @@
 import os
 import copy
 import random
+import augly.text as textaugs
 
 from typing import Text
 from arizona.textmentations.io import load_json
@@ -47,8 +48,6 @@ def abbreviates_func(
     }
 
     assert len(list_words) == len(list_tags), f"Error: `{text}`. The numbers words must be equal to the number of tags."
-
-    # _words = copy.deepcopy(list_words)
 
     for i in range(num_samples):
         for key, value in abbreviations_words.items():
@@ -110,15 +109,68 @@ def remove_accent_func(
         
         output_data["text"].append(textout)
         output_data["intent"].append(intent)
-        output_data["tags"].append(' '.join(tags))
+        output_data["tags"].append(tags)
 
     return output_data
-
 
 def keyboard_func(
     text: Text, 
     replace_thresold: float=0.5, 
+    num_samples: int=5, 
+    lowercase: bool=True, 
+    intent: Text=None, 
+    tags: Text=None, 
+    aug_char_percent: float=0.2,
+    aug_word_percent: float=0.1,
+    unikey_percent: float=0.5,
+    config_file: str='configs/unikey.json',
     **kwargs
 ):
-    
-    return 
+    unikey_numbers = int(unikey_percent)
+    keyboard_numbers = num_samples - unikey_numbers
+    viunikey = load_json(config_file)
+    output_data = {
+        "text": [],
+        "intent": [],
+        "tags": []
+    }
+
+    for i in range(len(unikey_numbers)):
+        textlist = []
+        for word in text.split(" "):
+            chartmp = ''
+            chars = [c for c in word]
+            rd = random.random()
+            if rd >= replace_thresold:
+                for i, c in enumerate(chars):
+                    if (c in viunikey.keys()):
+                        chars[i] = viunikey[c][0]
+                        chartmp = viunikey[c][1]
+                    if (i == len(chars) - 1):
+                        chars = ''.join(chars) + chartmp
+            
+                textlist.append(word)
+
+        textout = " ".join(textlist)
+        if lowercase:
+            textout = lowercase(textout)
+        
+        output_data["text"].append(textout)
+        output_data["intent"].append(intent)
+        output_data["tags"].append(tags)
+
+    keyboard_outputs = textaugs.simulate_typos(
+        text, 
+        aug_char_p=aug_char_percent, 
+        aug_word_p=aug_word_percent, 
+        n=keyboard_numbers
+    )
+    if lowercase:
+        keyboard_outputs = [lower_text(t) for t in keyboard_outputs]
+
+    for textout in keyboard_outputs:
+        output_data["text"].append(textout)
+        output_data["intent"].append(intent)
+        output_data["tags"].append(tags)
+
+    return output_data
