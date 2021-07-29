6663aaffbe75afedecc8fe00f97f358d2eda2870
phanxuanphucnd <phanxuanphucnd@gmail.com>
2021-07-29 09:50:08 +0700

    update tests

--- tests/test_keyboard_func.py
+++ tests/test_keyboard_func.py
@@ -7,7 +7,10 @@ results = keyboard_func(
     num_samples=5,
     intent='faq_company',
     tags='O O O B-work_unit I-word_unit I-work_unit',
-    config_file='configs/abbreviations.json',
+    aug_char_percent=0.2,
+    aug_word_percent=0.1,
+    unikey_percent=0.5,
+    config_file='configs/unikey.json',
 )
 
 from pprint import pprint
