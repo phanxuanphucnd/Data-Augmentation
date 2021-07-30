1f43ac5462fa0407e00ec379b7d8fa38c40fad1f
phanxuanphucnd <phanxuanphucnd@gmail.com>
2021-07-30 14:53:46 +0700

    update

--- tests/test_augmentation.py
+++ tests/test_augmentation.py
@@ -1,6 +1,6 @@
 from arizona.textmentations import TextAugmentation
 
-path = './data/nlu.csv'
+path = './data/train.csv'
 
 text_augs = TextAugmentation(
     data=path,
@@ -10,6 +10,30 @@ text_augs = TextAugmentation(
 )
 
 text_augs.augment(
-    methods=['remove_accent', 'abbreviation', 'keyboard'],
-    without_origin_data=True
+    methods={
+        "remove_accent": {
+            "replace_thresold": 0.3,
+            "num_samples": 10,
+            "lowercase": True
+        }, 
+        "abbreviation": {
+            "replace_thresold": 0.5,
+            "num_samples": 10,
+            "lowercase": True,
+            "config_file": "configs/abbreviations.json"
+            }, 
+        "keyboard": {
+            "replace_thresold": 0.5,
+            "num_samples": 10,
+            "lowercase": True,
+            "aug_char_percent": 0.2,
+            "aug_word_percent": 0.1,
+            "unikey_percent": 0.6,
+            "config_file": "configs/unikey.json"
+        }
+    },
+    without_origin_data=False,
+    write_file=True,
+    export_dir='./output',
+    export_file='train-aug.csv'
 )
